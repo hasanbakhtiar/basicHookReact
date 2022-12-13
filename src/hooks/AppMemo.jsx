d79d3841a114799fac5d327ccd9608777d0e601e
HAsan Bakhtiar <hasan.doctype@gmail.com>
2022-12-13 08:56:42 +0400

    useMemo

--- src/hooks/AppMemo.jsx
+++ src/hooks/AppMemo.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState,useMemo } from 'react'
+import userlist from '../data/users.json'
 
 const AppMemo = () => {
+
+  const [count, setCount] = useState(0);
+  const [userId,setUserId] = useState(null)
+  function getUserRows(){
+    console.log('getUserRows worked');
+    return userlist.map((user,index)=><div onClick={()=>{
+      setUserId(user.id)
+    }} style={{color: user.id === userId ? "red": "black"}} key={index}>{user.name}</div>)
+  }
+  
+
+  const userRows = useMemo(()=>getUserRows(),[userId]);
   return (
-    <div>AppMemo</div>
+    <div>
+      {userRows}
+      <button onClick={()=>{
+        setCount(count+1)
+      }}>add</button>
+      {userId}
+    </div>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,17 @@ import MovieList from './context/MovieList';
 import Nav from './context/Nav';
 import {MovieProvider} from './context/MovieContext';
 import AddMovies from './context/AddMovies';
+import AppMemo from './hooks/AppMemo'
 function App(){
-  return(<MovieProvider>
-    <Nav />
-    <AddMovies />
-    <MovieList/>
-  </MovieProvider>
-    
+  return(
+    <AppMemo />
   )
 }
 
-
+{/* <MovieProvider>
+    <Nav />
+    <AddMovies />
+    <MovieList/>
+  </MovieProvider> */}
 ReactDOM.render(<App />,document.getElementById('root'));
 
